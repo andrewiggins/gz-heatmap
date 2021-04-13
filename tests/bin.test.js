493df260a27d6463d7d9c0c1c6a2f18b9948583e
Andre Wiggins <andrewiggins@live.com>
2021-04-12 21:02:30 -0700

    Add some basic tests of the cmd line

--- tests/bin.test.js
+++ tests/bin.test.js
@@ -1,11 +1,14 @@
-import { existsSync } from "fs";
-import { readFile, mkdtemp } from "fs/promises";
+import { existsSync, writeFileSync } from "fs";
+import { readFile, writeFile, mkdtemp } from "fs/promises";
+import { execFileSync } from "child_process";
+import { join } from "path";
+import { pathToFileURL } from "url";
 import rimraf from "rimraf";
 import { suite } from "uvu";
 import * as assert from "uvu/assert";
-import { execFileSync } from "child_process";
+import puppeteer from "puppeteer";
+import prettier from "prettier";
 import { fixture, testPath } from "./utils/paths.js";
-import { join } from "path";
 
 /** @type {(path: string) => Promise<void>} */
 const rimrafAsync = (path) =>
@@ -14,16 +17,73 @@ const rimrafAsync = (path) =>
 	);
 
 const binPath = testPath("../bin/gz-heatmap.js");
+
+/**
+ * @typedef BinSuiteContext
+ * @property {import('puppeteer').Browser} browser
+ * @property {import('puppeteer').Page} page
+ * @property {string} output
+ */
+
+/**
+ * @type {import('uvu').Test<BinSuiteContext>}
+ */
 const bin = suite("bin");
+
+bin.before(async (ctx) => {
+	ctx.browser = await puppeteer.launch();
+});
 
 bin.before.each(async (ctx) => {
 	ctx.output = await mkdtemp("gz-heatmap-tests-");
+	ctx.page = await ctx.browser.newPage();
 });
 
 bin.after.each(async (ctx) => {
+	await ctx.page.close();
 	await rimrafAsync(ctx.output);
-	ctx.output = null;
+	ctx.output = "";
 });
+
+bin.after(async (ctx) => {
+	await ctx.browser.close();
+});
+
+/** @type {(outputDir: string) => string} */
+const getIndexUrl = (outputDir) =>
+	pathToFileURL(join(outputDir, "index.html")).toString();
+
+/** @type {(rawHtml: string) => string} */
+function formatHtml(rawHtml) {
+	rawHtml = rawHtml
+		.replace(/([^\n])<span /g, "$1\n<span ")
+		.replace(/([^\n])<\/pre>/g, "$1\n</pre>");
+
+	return prettier.format(rawHtml, { parser: "html" });
+}
+
+/**
+ * @param {BinSuiteContext} ctx
+ * @param {string} fixtureDir
+ * @param {string} inputFile
+ */
+async function validateFixture(ctx, fixtureDir, inputFile) {
+	const inputFixture = fixture(fixtureDir, inputFile);
+	execFileSync("node", [binPath, inputFixture, "--out", ctx.output]);
+
+	await ctx.page.goto(getIndexUrl(ctx.output), { waitUntil: "networkidle2" });
+	const html = formatHtml(await ctx.page.content());
+
+	const expectedFixture = fixture(fixtureDir, "expected.html");
+	await writeFile(expectedFixture, html, "utf8");
+	const expectedHtml = await readFile(expectedFixture, "utf8");
+
+	assert.fixture(
+		html,
+		expectedHtml,
+		`output doesn't match expected for "${inputFixture}"`
+	);
+}
 
 bin("HTML should only reference files that exist", async (ctx) => {
 	const fixturePath = fixture("svg-7-hex/image.svg");
@@ -39,16 +99,20 @@ bin("HTML should only reference files that exist", async (ctx) => {
 	}
 });
 
-bin("should handle gz input", (ctx) => {
-	const fixturePath = fixture("svg-7-hex/image.svg.gz");
-	execFileSync("node", [binPath, fixturePath, "--out", ctx.output]);
-	// TODO: assert that the JS/HTML file contains results. Maybe use puppeteer?
+bin("should handle gz input", async (ctx) => {
+	await validateFixture(ctx, "svg-7-hex", "image.svg.gz");
 });
 
-bin("should handle xml input", (ctx) => {});
+bin("should handle xml input", async (ctx) => {
+	await validateFixture(ctx, "svg-7-hex", "image.svg");
+});
 
-bin("should handle text input", (ctx) => {});
+bin("should handle text input", async (ctx) => {
+	await validateFixture(ctx, "simple", "simple.txt");
+});
 
-bin("should handle js input", (ctx) => {});
+bin("should handle js input", async (ctx) => {
+	// await validateFixture(ctx, "svg-7-hex", "image.svg.gz");
+});
 
 bin.run();
